2a831decf1636ff30ad778bfc089dbdd04b10352
Felipe G <pipe2705@gmail.com>
2019-11-26 11:10:15 -0800

    Creates the table for Jobs and alters the database.js file

--- database.js
+++ database.js
@@ -5,9 +5,17 @@ let database = new sqlite3.Database("./database.db");
 const createTableJobSeekerQuery =
   "CREATE TABLE IF NOT EXISTS JobSeeker (name TEXT, city_location TEXT, profession_id INTEGER, age INTEGER, years_of_experience INTEGER, country_location TEXT)";
 
+const createTableJobsQuery =
+  "CREATE TABLE IF NOT EXISTS Jobs (position TEXT, company TEXT, salary INTEGER, city_location TEXT, country_location TEXT, profession_id INTEGER, date_posted INTEGER) ";
+
 database.run(createTableJobSeekerQuery, error => {
   if (error) console.log(new Error("Create Job Seeker table failed"), error);
   else console.log("Create new Job Seeker table succeeded!");
+});
+
+database.run(createTableJobsQuery, error => {
+  if (error) console.log(new Error("Create Jobs table failed"), error);
+  else console.log("Create new Jobs table succeeded!");
 });
 
 module.exports = database;
