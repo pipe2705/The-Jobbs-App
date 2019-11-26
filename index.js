38f1a3717d74d88c05cc57bfc81acb9f4c698768
Felipe G <pipe2705@gmail.com>
2019-11-25 20:31:37 -0800

    Second commit adds Express to index.js including requirement statement. package.json is updated with express dependencies.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello test") 
+let express = require("express");
+
+let app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  response.send("To see the job listing visit /api/jobs");
+});
