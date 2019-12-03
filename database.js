0c9215bac2718fe55d0570ac5a4138536ad9e74c
Felipe G <pipe2705@gmail.com>
2019-12-02 18:39:18 -0800

    Fixes Bug with db instead of database in the database,serialize function

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ let sqlite3 = require("sqlite3");
 
 let database = new sqlite3.Database("./database.db");
 
-db.serialize(() => {
+database.serialize(() => {
   const createTableJobSeekerQuery =
     "CREATE TABLE IF NOT EXISTS JobSeeker (name TEXT, city_location TEXT, profession_id INTEGER, age INTEGER, years_of_experience INTEGER, country_location TEXT)";
 
